f48d3ccc027a0f3f780b5badf736aa0a0118a748
Mlie <emipa606@users.noreply.github.com>
2022-08-13 20:12:23 +0200

    Sentient animals should no longer try to roam

---
+++ Source/SentientAnimals/CanRoamNow_Patch.cs
@@ -0,0 +1,17 @@
+﻿using HarmonyLib;
+using Verse;
+using Verse.AI;
+
+namespace SentientAnimals;
+
+[HarmonyPatch(typeof(MentalStateWorker_Roaming), "CanRoamNow")]
+public static class CanRoamNow_Patch
+{
+    public static void Postfix(Pawn pawn, ref bool __result)
+    {
+        if (pawn.IsSentient())
+        {
+            __result = false;
+        }
+    }
+}
